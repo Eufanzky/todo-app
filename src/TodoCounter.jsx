01a0793f2a0f320d5c5e36a6d269b3a2cc240951
eufanzky <eufanzky@gmail.com>
2022-12-16 17:58:53 -0400

    locacl storage added

--- src/TodoCounter.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import './TodoCounter.css';
-
-export function TodoCounter({ numberOfCompletedTodos, numberOfTotalTodos }) {
-  return <h2 className="todoCounter">You have completed {numberOfCompletedTodos} of {numberOfTotalTodos} tasks</h2>;
-}
-
